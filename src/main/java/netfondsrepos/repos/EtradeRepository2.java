492b0b10eadc00d86ef4822947692b97b0ba3e1b
hamilton108 <hamilton108@palenque.com>
2018-11-28 00:52:50 +0100

    Jsoup calls

--- src/main/java/netfondsrepos/repos/EtradeRepository2.java
+++ src/main/java/netfondsrepos/repos/EtradeRepository2.java
@@ -1,6 +1,7 @@
 package netfondsrepos.repos;
 
 import com.gargoylesoftware.htmlunit.Page;
+import critterrepos.beans.options.DerivativePriceBean;
 import oahu.dto.Tuple;
 import oahu.dto.Tuple3;
 import oahu.financial.Derivative;
@@ -8,14 +9,15 @@ import oahu.financial.DerivativePrice;
 import oahu.financial.StockPrice;
 import oahu.financial.html.EtradeDownloader;
 import oahu.financial.repository.EtradeRepository;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Optional;
+import java.util.*;
 
 public class EtradeRepository2 implements
         EtradeRepository<Tuple<String>,
@@ -23,7 +25,8 @@ public class EtradeRepository2 implements
 
     private EtradeDownloader<Page, Serializable> downloader;
 
-    private Collection<DerivativePrice> derivativePrices;
+    private Map<String,Tuple3<Optional<StockPrice>,Collection<DerivativePrice>,Collection<DerivativePrice>>>
+    stoxPutsCalls = null;
 
     //region interface EtradeRepository
     @Override
@@ -59,11 +62,59 @@ public class EtradeRepository2 implements
     @Override
     public Tuple3<Optional<StockPrice>, Collection<DerivativePrice>, Collection<DerivativePrice>>
     parseHtmlFor(String ticker, File suppliedFile) {
-        return null;
+        try {
+            if (stoxPutsCalls == null) {
+                stoxPutsCalls = new HashMap<>();
+            }
+            Tuple3<Optional<StockPrice>, Collection<DerivativePrice>, Collection<DerivativePrice>>
+                    result = stoxPutsCalls.get(ticker);
+            if (result == null) {
+                Page page = downloader.downloadDerivatives(ticker);
+                Document doc = Jsoup.parse(page.getWebResponse().getContentAsString());
+                Element top = doc.getElementsByClass("com topmargin").first();
+                //List<Element> trs = top.getElementsByTag("tr");
+                List<Element> rawCalls = top.getElementsContainingOwnText("American call");
+                List<Element> rawPuts = top.getElementsContainingOwnText("American put");
+
+                Collection<DerivativePrice> calls = new ArrayList<>();
+                for (Element el : rawCalls) {
+                    DerivativePrice call = processElement(el.parent());
+                    if (call != null) {
+                        calls.add(call);
+                    }
+                }
+                result = new Tuple3<>(Optional.empty(), calls, null);
+            }
+            return result;
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    private DerivativePrice processElement(Element el) {
+        int sz = el.childNodeSize();
+        if (sz < 15) {
+            return null;
+        }
+        Element buyEl = el.child(4);
+        Element sellEl = el.child(5);
+
+        try {
+            double buy = Double.parseDouble(buyEl.text());
+            double sell = Double.parseDouble(sellEl.text());
+            DerivativePrice bean = new DerivativePriceBean(null, buy, sell, null);
+            return bean;
+        }
+        catch (NumberFormatException ex) {
+            return null;
+        }
     }
     //endregion
 
     //region Private Methods
+
+    /*
     private Collection<DerivativePrice> getDerivatives() {
         if (derivativePrices == null) {
             derivativePrices = new ArrayList<>();
@@ -90,6 +141,7 @@ public class EtradeRepository2 implements
     public DerivativePrice fromStringRow(String row) {
         return null;
     }
+    //*/
     //endregion
 
     //region Properties
